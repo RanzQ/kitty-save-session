1289f7be0bec11636eb775a46dd8fb9d78135050
RanzQ <juha@codercoded.com>
2023-09-13 09:15:03 +0300

    Move convert script to kitty-dump.sh

--- kitty-dump.sh
+++ kitty-dump.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # Dump the current kitty session:
 kitty @ ls > ~/.kitty-dump.json
+
 # Restrict access to it
 chmod 600 ~/.kitty-dump.json
+
+# Convert the kitty dump JSON file into a kitty session file:
+cat ~/.kitty-dump.json | python3 ~/scripts/kitty-convert-dump.py > ~/.kitty-session.kitty
+
+# Delete the json
+rm ~/.kitty-dump.json
